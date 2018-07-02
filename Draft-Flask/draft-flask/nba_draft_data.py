58092c7578c4c2fb0434dfaa8e9f86bd2d031039
Richard Song <brsong22@gmail.com>
2018-07-02 12:26:31 -0500

    update draft dict structure
    rework draft dictionary strucutre to be easier to read and work with.

--- Draft-Flask/draft-flask/nba_draft_data.py
+++ Draft-Flask/draft-flask/nba_draft_data.py
@@ -55,97 +55,110 @@ for i in range(5):
 	team_stats_files.append('[' + file_year + '] Team Draft Order and Stats Totals.csv')
 
 drafts_data = []
-players_data = []
-teams_data = []
 
-for y, f in enumerate(player_files):
-	with open(f, newline='') as player_file:
-		print(prev_year-y)
-		stats_headers = []
-		player_stat_rows = csv.reader(player_file, delimiter=',')
-		player_stat_totals = {}
-		for row_index, stat_row in enumerate(player_stat_rows):
-			if row_index == 0:
-				stats_headers = stat_row
-			else:
-				if not prev_year-y in player_stat_totals:
-					player_stat_totals[prev_year-y] = {}
-				player_stat_totals[prev_year-y][stat_row[0]] = {stat_row[1]:{
-															   	stats_headers[2]: stat_row[2],
-															   	stats_headers[3]: stat_row[3],
-															   	stats_headers[4]: stat_row[4],
-															   	stats_headers[5]: stat_row[5],
-															   	stats_headers[6]: stat_row[6],
-															   	stats_headers[7]: stat_row[7],
-															   	stats_headers[8]: stat_row[8],
-															   	stats_headers[9]: stat_row[9],
-															   	stats_headers[10]: stat_row[10],
-															   	stats_headers[11]: stat_row[11],
-															   	stats_headers[12]: stat_row[12],
-															   	stats_headers[13]: stat_row[13]}}
-		players_data.append(player_stat_totals)
-	player_file.close()
+def get_draft_data():
+	for i in range(5):
+		year_i = prev_year - i
+		draft_i = {}
+		draft_i[year_i] = {}
+		file_year = str(year_i)
+		player_stat_file = '[' + file_year +'] Drafted Player Stats.csv'
+		team_stat_rank_file = '[' + file_year + '] Team Draft Order and Stats Rankings.csv'
+		team_stat_total_file = '[' + file_year + '] Team Draft Order and Stats Totals.csv'
+		#get player stats for each draft
+		#[{year:{pick:{name:{stats}}}}]
+		with open(player_stat_file, newline='') as player_file:
+			stats_headers = []
+			player_stat_rows = csv.reader(player_file, delimiter=',')
+			player_stat_totals = {}
+			for row_index, stat_row in enumerate(player_stat_rows):
+				if row_index == 0:
+					stats_headers = stat_row
+				else:
+					player_stat_totals[row_index] = {stats_headers[1]: stat_row[1],
+													 stats_headers[2]: stat_row[2],
+													 stats_headers[3]: stat_row[3],
+													 stats_headers[4]: stat_row[4],
+													 stats_headers[5]: stat_row[5],
+													 stats_headers[6]: stat_row[6],
+													 stats_headers[7]: stat_row[7],
+													 stats_headers[8]: stat_row[8],
+													 stats_headers[9]: stat_row[9],
+													 stats_headers[10]: stat_row[10],
+													 stats_headers[11]: stat_row[11],
+													 stats_headers[12]: stat_row[12],
+													 stats_headers[13]: stat_row[13]}
+		player_file.close()
+		draft_i[year_i]['players'] = player_stat_totals
 
-team_stats_data = {}
-for y, f in enumerate(team_ranks_files):
-	with open(f, newline='') as team_rank_file:
-		draft_order = []
-		stats_headers = []
-		team_stat_rows = csv.reader(team_rank_file, delimiter=',')
-		team_stat_ranks = {}
-		for row_index, stat_row in enumerate(team_stat_rows):
-			if row_index == 0:
-				stats_headers = stat_row
-			else:
-				team_abbr = team_abbr_lookup.get(stat_row[1])
-				draft_order.append(team_abbr)
-				team_stat_ranks[team_abbr] = {stats_headers[1]: stat_row[1],
-											  stats_headers[2]: stat_row[2],
-											  stats_headers[3]: stat_row[3],
-											  stats_headers[4]: stat_row[4],
-											  stats_headers[5]: stat_row[5],
-											  stats_headers[6]: stat_row[6],
-											  stats_headers[7]: stat_row[7],
-											  stats_headers[8]: stat_row[8],
-											  stats_headers[9]: stat_row[9],
-											  stats_headers[10]: stat_row[10],
-											  stats_headers[11]: stat_row[11],
-											  stats_headers[12]: stat_row[12],
-											  stats_headers[13]: stat_row[13]}
-		team_stats_data[prev_year-y] = {'order': draft_order, 'ranks': team_stat_ranks}
-	team_rank_file.close()
+		team_stats_data = {}
+		#get team stats ranks for each draft
+		#{year:{order:[{name, abbr}], ranks:{team:{stats}}}}
+		with open(team_stat_rank_file, newline='') as team_rank_file:
+			draft_order = {}
+			stats_headers = []
+			team_stat_rows = csv.reader(team_rank_file, delimiter=',')
+			team_stat_ranks = {}
+			for row_index, stat_row in enumerate(team_stat_rows):
+				if row_index == 0:
+					stats_headers = stat_row
+				else:
+					team_abbr = team_abbr_lookup.get(stat_row[1])
+					player_pick = draft_i[year_i]['players'][row_index].get('name')
+					draft_order[row_index] = {'team': stat_row[1], 'abbr': team_abbr, 'player': player_pick}
+					team_stat_ranks[team_abbr] = {stats_headers[1]: stat_row[1],
+												  stats_headers[2]: stat_row[2],
+												  stats_headers[3]: stat_row[3],
+												  stats_headers[4]: stat_row[4],
+												  stats_headers[5]: stat_row[5],
+												  stats_headers[6]: stat_row[6],
+												  stats_headers[7]: stat_row[7],
+												  stats_headers[8]: stat_row[8],
+												  stats_headers[9]: stat_row[9],
+												  stats_headers[10]: stat_row[10],
+												  stats_headers[11]: stat_row[11],
+												  stats_headers[12]: stat_row[12],
+												  stats_headers[13]: stat_row[13]}
+		team_rank_file.close()
+		draft_i[year_i]['order'] = draft_order
 
-for y, f in enumerate(team_stats_files):
-	with open(f, newline='') as team_totals_file:
-		stats_headers = []
-		team_stat_rows = csv.reader(team_totals_file, delimiter=',')
-		team_stat_totals = {}
-		for row_index, stat_row in enumerate(team_stat_rows):
-			if row_index == 0:
-				stats_headers = stat_row
-			else:
-				team_abbr = team_abbr_lookup.get(stat_row[1])
-				team_stat_totals[team_abbr] = {stats_headers[1]: stat_row[1],
-											   stats_headers[2]: stat_row[2],
-											   stats_headers[3]: stat_row[3],
-											   stats_headers[4]: stat_row[4],
-											   stats_headers[5]: stat_row[5],
-											   stats_headers[6]: stat_row[6],
-											   stats_headers[7]: stat_row[7],
-											   stats_headers[8]: stat_row[8],
-											   stats_headers[9]: stat_row[9],
-											   stats_headers[10]: stat_row[10],
-											   stats_headers[11]: stat_row[11],
-											   stats_headers[12]: stat_row[12],
-											   stats_headers[13]: stat_row[13]}
-		team_stats_data[prev_year-y]['totals'] = team_stat_totals
-	team_totals_file.close()
+		#get team stats totals for each draft
+		#{abbr:{stats}}
+		with open(team_stat_total_file, newline='') as team_totals_file:
+			stats_headers = []
+			team_stat_rows = csv.reader(team_totals_file, delimiter=',')
+			team_stat_totals = {}
+			for row_index, stat_row in enumerate(team_stat_rows):
+				if row_index == 0:
+					stats_headers = stat_row
+				else:
+					team_abbr = team_abbr_lookup.get(stat_row[1])
+					team_stat_totals[team_abbr] = {stats_headers[1]: stat_row[1],
+												   stats_headers[2]: stat_row[2],
+												   stats_headers[3]: stat_row[3],
+												   stats_headers[4]: stat_row[4],
+												   stats_headers[5]: stat_row[5],
+												   stats_headers[6]: stat_row[6],
+												   stats_headers[7]: stat_row[7],
+												   stats_headers[8]: stat_row[8],
+												   stats_headers[9]: stat_row[9],
+												   stats_headers[10]: stat_row[10],
+												   stats_headers[11]: stat_row[11],
+												   stats_headers[12]: stat_row[12],
+												   stats_headers[13]: stat_row[13]}
+			#add stats totals dict to team stats dict
+		team_totals_file.close()
 
-for key in team_stats_data:
-	teams_data.append({'year': key, 'stats': team_stats_data.get(key)})
-drafts_data.append({'players': players_data, 'teams': teams_data})
+		team_stats = {}
+		for key in team_stat_ranks:
+			team_stats[key] = {}
+			team_stats[key]['ranks'] = team_stat_ranks.get(key)
+			team_stats[key]['totals'] = team_stat_totals.get(key)
+		draft_i[year_i]['teams'] = team_stats
+		drafts_data.append(draft_i)
 
-print(drafts_data)
-return drafts_data
-
-
+	#drafts_data = [{year{player, order, team}}]
+	#	#player = {pick{name, stats}}
+	#	#order = {pick{team, abbr, player}}
+	#	#team = {abbr: ranks{stats}, totals{stats}}
+	return drafts_data
