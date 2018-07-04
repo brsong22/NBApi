03e0ce4ed858d0212531f3c48e1e2d8181cc4fb7
Richard Song <brsong22@gmail.com>
2018-07-04 12:24:34 -0500

    create form components
    create form components for form to submit to get api parameters

--- Draft-React/draft-react/src/App.js
+++ Draft-React/draft-react/src/App.js
@@ -2,84 +2,115 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class DraftYearSelector extends Component{
+  static currentYear = new Date().getFullYear();
 
-class NBApi extends Component{
   constructor(props){
     super(props);
     this.state = {
-      year: new Date().getFullYear(),
-      pick: 0,
-      getStats: false
+      year: new Date().getFullYear()-1
     };
-
     this.handleInputChange = this.handleInputChange.bind(this);
   }
-
+  
   handleInputChange(event){
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
+    this.setState({year: event.target.value});
   }
 
   render(){
-    const year = this.state.year;
-    const pick = this.state.pick;
-    const getStats = this.state.getStats;
+    const draftYears = [...Array(5).keys()].map(
+      value => <option key={(value+1).toString()} value={DraftYearSelector.currentYear - (value+1)}>
+        {DraftYearSelector.currentYear - (value+1)}
+      </option>);
+    return(
+      <label>
+        Select Draft Year:
+        <select name="year" value={this.state.year} onChange={this.handleInputChange}>
+          {draftYears}
+        </select>
+      </label>
+    );
+  }
+}
 
-    const pickNumOptions = [...Array(30).keys()].map(value => <option value={value+1}>{value+1}</option>);
+class DraftPickSelector extends Component{
+  static numPicks = 30;
+
+  constructor(props){
+    super(props);
+    this.state = {
+      pick: 1
+    };
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange(event){
+    this.setState({pick: event.target.value});
+  }
+
+  render(){
+    const pickNumList = [...Array(DraftPickSelector.numPicks).keys()].map(
+      value => <option key={(value+1).toString()} value={value+1}>{value+1}</option>);
+    return(
+      <label>
+        Select Pick Number:
+        <select name="pick" value={this.state.pick} onChange={this.handleInputChange}>
+          {pickNumList}
+        </select>
+      </label>
+    );
+  }
+}
+
+class DraftStatsSelector extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      getStats: false
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  handleInputChange(event){
+    this.setState({getStats: event.target.value});
+  }
+  render(){
+    return(
+      <label>
+        Show Draft Pick Stats?:
+        <select name="getStats" value={this.state.getStats} onChange={this.handleInputChange}>
+          <option key="0" value="false">No</option>
+          <option key="1" value="true">Yes</option>
+        </select>
+      </label>
+    );
+  }
+}
+
+class NBApiForm extends Component{
+  constructor(){
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    const data = new FormData(event.target);
+    console.log(data);
+  }
+  render(){
     return(
       <div>
-        <label>
-          Select Draft Year:
-          <select name="year" value={this.state.year} onChange={this.handleInputChange}>
-            <option value="2017">2017</option>
-            <option value="2016">2016</option>
-            <option value="2015">2015</option>
-            <option value="2014">2014</option>
-            <option value="2013">2013</option>
-          </select>
-        </label>
-        <br/>
-        <label>
-          Select Draft Pick:
-          <select name="pick" value={this.state.pick} onChange={this.handleInputChange}>
-            {pickNumOptions}
-          </select>
-        </label>
-        <br/>
-        <label>
-          Show Draft Pick Stats?:
-          <select name="getStats" value={this.state.getStats} onChange={this.handleInputChange}>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-        </label>
+        <form onSubmit={this.handleSubmit}>
+          <DraftYearSelector/>
+          <br/>
+          <DraftPickSelector/>
+          <br/>
+          <DraftStatsSelector/>
+          <br/>
+          <input type="submit" value="Get Draft Info"/>
+        </form>
       </div>
     );
   }
 }
 
-export default NBApi;
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//           <br/>hello
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
+export default NBApiForm;
--- Draft-React/draft-react/src/index.js
+++ Draft-React/draft-react/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import NBApi from './App';
+import NBApiForm from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<NBApi />, document.getElementById('root'));
+ReactDOM.render(<NBApiForm />, document.getElementById('root'));
 registerServiceWorker();
